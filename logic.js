4c89f46f82c5a75f62797d09014ae9e00940e603
Ardit-K <arditkoti7192@gmail.com>
2024-11-08 13:37:09 -0500

    feat(index.html/css) Finished making grid.

--- logic.js
+++ logic.js
@@ -1,6 +1,11 @@
 let container = document.querySelector(".container");
-for (let i = 0; i < 16; i++){
-    let d = document.createElement("div");
-    d.setAttribute("class", "box");
-    container.appendChild(d);
+for (let i = 0; i < 4; i++){
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 4; j++){
+        let d = document.createElement("div");
+        d.classList.add("box");
+        row.appendChild(d);
+    }
+    container.appendChild(row);
 }

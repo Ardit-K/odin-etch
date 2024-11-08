184d494be2c61aaeed44f852b571f1f3f295a236
Ardit-K <arditkoti7192@gmail.com>
2024-11-08 13:44:49 -0500

    feat(logic.js) Ability to change box color.

--- logic.js
+++ logic.js
@@ -5,6 +5,9 @@ for (let i = 0; i < 4; i++){
     for (let j = 0; j < 4; j++){
         let d = document.createElement("div");
         d.classList.add("box");
+        d.addEventListener('mouseover', ()=>{
+            d.style.backgroundColor = 'black';
+        });
         row.appendChild(d);
     }
     container.appendChild(row);

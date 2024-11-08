da7dcfdcc3605d15c1053273d40aa89e0b80b689
Ardit-K <arditkoti7192@gmail.com>
2024-11-08 14:19:16 -0500

    feat(logic.js) Randomized box colors.

--- logic.js
+++ logic.js
@@ -11,7 +11,10 @@ const newGrid = () => {
             let d = document.createElement("div");
             d.classList.add("box");
             d.addEventListener('mouseover', ()=>{
-                d.style.backgroundColor = 'black';
+                d.style.backgroundColor = `rgb(
+                ${(Math.random() * 1000) % 255},
+                ${(Math.random() * 1000) % 255},
+                ${(Math.random() * 1000) % 255})`
             });
             row.appendChild(d);
         }

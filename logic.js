e980a25e2a82d626b11ce522f1e232245de6cb78
Ardit-K <arditkoti7192@gmail.com>
2024-11-08 13:57:54 -0500

    Feats: Added customizable square amounts.

--- logic.js
+++ logic.js
@@ -1,14 +1,20 @@
 let container = document.querySelector(".container");
-for (let i = 0; i < 4; i++){
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 4; j++){
-        let d = document.createElement("div");
-        d.classList.add("box");
-        d.addEventListener('mouseover', ()=>{
-            d.style.backgroundColor = 'black';
-        });
-        row.appendChild(d);
+
+const newGrid = () => {
+    let squares = prompt("Enter number of squares per side (maximum 100): ");
+    if (squares > 100) newGrid();
+    container.innerHTML = '';
+    for (let i = 0; i < squares; i++){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < squares; j++){
+            let d = document.createElement("div");
+            d.classList.add("box");
+            d.addEventListener('mouseover', ()=>{
+                d.style.backgroundColor = 'black';
+            });
+            row.appendChild(d);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
